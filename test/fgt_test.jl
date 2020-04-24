6c2579c1e0cc7501c96757c5a05267e3077646d7
Patrick Eastham <peastham@math.fsu.edu>
2020-04-24 10:35:04 -0400

    modified test to hopefully work

--- test/fgt_test.jl
+++ test/fgt_test.jl
@@ -1,25 +1,23 @@
 using StokesParticles, Test
 import LinearAlgebra: norm
 
-d = 2                           # dimension of data
-M = 1_000                       # number of targets 
-N = 1_000                       # number of sources
-h = 0.8                         # bandwidth
-ε = 1e-2                        # max tolerance
-x = rand(N*d)                   # source vectors, reshape'd to 1D array
-y = rand(M*d)                   # target vectors, reshape'd to 1D array
-W = 1                           # number of sources being evaluated 
-q = rand(N*W)                   # weights for sources
-v1 = zeros(M)                   # output values array
-v2 = zeros(M)                   # output 2
-v3 = zeros(M)                   # output 3
+d = 1                                   # dimension of data
+M = 10_000                              # number of targets 
+N = 10_000                              # number of sources
+h = 0.2                                 # bandwidth
+ε = 1e-2                                # max tolerance
+x = rand(N*d)                           # source vectors, reshape'd to 1D array
+y = collect(range(-2,stop=2,length=M))  # target vectors, reshape'd to 1D array
+W = 1                                   # number of sources being evaluated 
+q = rand(N*W)                           # weights for sources
+v1 = rand(M)                            # output values array
+v2 = rand(M)                            # output 2
+v3 = rand(M)                            # output 3
 
-# compile
-fgt!(v1,d,M,N,h,ε,x,y,q,W)
-dgt!(v1,d,M,N,h,ε,x,y,q,W)
+t1 = @elapsed fgt!(v1,d,M,N,h,ε,x,y,q,W)
+t2 = @elapsed dgt!(v2,d,M,N,h,ε,x,y,q,W)
+t3 = @elapsed mydgt!(v3,d,M,N,h,ε,x,y,q,W)
 
-t1 = @elapsed fgt!(v1,d,M,N,h,ε,x,y,q,W)        # fast c++
-t2 = @elapsed dgt!(v2,d,M,N,h,ε,x,y,q,W)        # slow c++
-
-@test t1 < t2
-@test norm(v1-v2) < eps()
+@test t1 < t2 < t3
+@test norm(v1 - v2) < eps()
+@test norm(v1 - v3) < eps()
