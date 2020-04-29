ad5daa011563a3bbd4442825f82ecbad8c0fc2ad
Patrick Eastham <peastham@math.fsu.edu>
2020-04-29 09:54:06 -0400

    fixed test bug

--- test/cohesion_test.jl
+++ test/cohesion_test.jl
@@ -4,8 +4,8 @@ d = 1.3
 ϵ = 0.1
 s = 0.3
 
-@test ( StokesParticles.LJForceMagnitude(1.5,s,d,ϵ) == 0)
+@test ( StokesParticles.LJForceMagnitude((1+2*ϵ)*d,s,d,ϵ) == 0)
 @test ( StokesParticles.LJForceMagnitude(d,s,d,ϵ) == 0)
-@test ( StokesParticles.LJForceMagnitude(0.5*d,s,d,ϵ) > 0 )
+@test ( StokesParticles.LJForceMagnitude((1-ϵ)*d,s,d,ϵ) > 0 )
 @test ( StokesParticles.LJForceMagnitude((1+ϵ/2)*d,s,d,ϵ) < 0 )
 
