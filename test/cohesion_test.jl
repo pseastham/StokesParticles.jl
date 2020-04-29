2ca8f86bcad6323fda290a7074cf09e1852bd3f1
Patrick Eastham <peastham@math.fsu.edu>
2020-04-29 09:59:37 -0400

    changed back

--- test/cohesion_test.jl
+++ test/cohesion_test.jl
@@ -6,6 +6,6 @@ s = 0.3
 
 @test ( StokesParticles.LJForceMagnitude((1+2*ϵ)*d,s,d,ϵ) == 0)
 @test ( StokesParticles.LJForceMagnitude(d,s,d,ϵ) == 0)
-@test ( StokesParticles.LJForceMagnitude((1-ϵ)*d,s,d,ϵ) > 0 )
-@test ( StokesParticles.LJForceMagnitude((1+ϵ/2)*d,s,d,ϵ) < 0 )
+@test ( StokesParticles.LJForceMagnitude((1-ϵ)*d,s,d,ϵ) < 0 )
+@test ( StokesParticles.LJForceMagnitude((1+ϵ/2)*d,s,d,ϵ) > 0 )
 
