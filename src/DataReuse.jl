f78f0b5dd8618a6dd2772aafb381a4d447231dbb
Patrick Eastham <peastham@math.fsu.edu>
2020-05-01 16:31:24 -0400

    fixed sh_data members

--- src/DataReuse.jl
+++ src/DataReuse.jl
@@ -1,21 +1,27 @@
 mutable struct sh_data  # sinkhole data
-    polygon::Vector      = [Point2D(0.0,0.0),Point2D(0.0,0.0),Point2D(0.0,0.0),Point2D(0.0,0.0)]
-    extremePoint = Point2D(100_000.0,0.0)
-    pointOnWall = Point2D(0.0,0.0)
-    xquad = zeros(Float64,Nint); yquad = zeros(Float64,Nint)
-    cfX = zeros(Float64,Nparticles); cfY = zeros(Float64,Nparticles)
-    afX = zeros(Float64,Nparticles); afY = zeros(Float64,Nparticles)
+    polygon::Vector{Point2D{Float64}}
+    extremePoint::Vector{Point2D{Float64}}
+    pointOnWall::Point2D{Float64}
+    xquad::Vector{Float64}
+    yquad::Vector{Float64}
+    cfX::Vector{Float64}
+    cfY::Vector{Float64}
+    afX::Vector{Float64}
+    afY::Vector{Float64}
 
     function sh_data(n_quad::Int,n_particles::Int)
         polygon      = [Point2D(0.0,0.0),Point2D(0.0,0.0),Point2D(0.0,0.0),Point2D(0.0,0.0)]
         extremePoint = Point2D(100_000.0,0.0)
         pointOnWall  = Point2D(0.0,0.0)
-        xquad        = zeros(Nint)
-        yquad        = zeros(Nint)
-        cfX          = zeros(Nparticles)
-        cfY          = zeros(Nparticles)
-        afX          = zeros(Nparticles)
-        afY          = zeros(Nparticles)
+        xquad        = zeros(n_quad)
+        yquad        = zeros(n_quad)
+        cfX          = zeros(n_particles)
+        cfY          = zeros(n_particles)
+        afX          = zeros(n_particles)
+        afY          = zeros(n_particles)
         return new(polygon,extremePoint,pointOnWall,xquad,yquad,cfX,cfY,afX,afY)
+    end
+    function sh_data(n_particles::Int)
+        return sh_data(1,n_particles)
     end
 end 
